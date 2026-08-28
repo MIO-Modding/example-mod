04583c84e626f08369394fdbec3aff5eeca05305
Cmdpro <73263645+Cmdpro@users.noreply.github.com>
2026-08-28 00:09:25 -0400

    c#

---
+++ ExampleMod.cs
@@ -0,0 +1,37 @@
+﻿using MioGame;
+using MioModdingApi;
+using MioModLoader;
+using PolyHook2.API;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+namespace TestMod
+{
+    public class ExampleMod : Mod
+    {
+        public override void Initialize()
+        {
+            Hooks();
+
+            //Example .gin Patch
+            GinPatching.PatchGins += () =>
+            {
+                GinPatching.AddGinPatch("flamby/assets.gin", Path.Combine(GetModFolderPath(), "assets_override.gin"));
+            };
+        }
+        private unsafe void Hooks()
+        {
+            //Example Hook
+            On.MioGame.On_Game.fixed_update.Prefix += Fixed_update_Prefix;
+        }
+
+        private unsafe void Fixed_update_Prefix(MioGame.Game* __this)
+        {
+            var mio = __this->mio;
+            if (mio.node != null && !mio.cutscene.active && !mio.walk_bot.active && mio.hook.state._value == MioGame.Mio.Hook.State.Inactive)
+            {
+                mio.move_by_slide(new MioGame.Vec_float_3() { Base = new MioGame._vec_storage_float_3() { x = 0.1f } });
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-## THIS IS FOR MIO MODDING API 0.1.0
+# Setup
+## Changing things for your mod
+Open example-mod.sln in your ide, then rename the solution, csproj and .cs for your mod
+After that, open up the Mod folder and open the mod.json and change `id` to your mods id, `name` to your mods name, `main` to the name of the dll you will be using for your mod, and if you have any additional dependencies, you can add them to `dependencies`
+
+## Existing Code
+Within the Mod folder, there is an `assets_override.gin`, this is used in the code with the GinPatching.AddGinPatch method call in the .cs file
+Within the .cs file there also is an example hook, this hook will move the player slightly to the right every frame
+
+# Building
+Building the mod works the same as any other c# project, however the output for the mod will be at `bin/<YOURMODNAME>`, copy this folder into your mods folder to run your mod
+If you wish to make a symlink, you can open up the command prompt, navigate to your mods folder, and run `mklink /J <YOURMODNAME> "<PATHTOMOD>/bin/<YOURMODNAME>"`
